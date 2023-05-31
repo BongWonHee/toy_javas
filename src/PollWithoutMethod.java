939d448cb5c31f5f2cda5e8c392d6cd74d3abb72
BongWonHee <leeban1004@gmail.com>
2023-05-31 12:26:21 +0900

    delete class

--- src/PollWithoutMethod.java
+++ src/PollWithoutMethod.java
@@ -11,22 +11,22 @@ public class PollWithoutMethod {
         int con = 0;
         String[][] Que = {
                 { "1.교수는 수업 전 강의 목표를 명확히 제시하였습니까?" },
-                { "(1)전혀 아니다", "(2)아니다.", "(3)그렇다.", "(4)매우그렇다." },
+                { "(1)전혀 아니다 (2)아니다. (3)그렇다. (4)매우그렇다." },
                 { "2.강의의 내용은 체계적이고 성의있게 구성되었는가?" },
-                { "(1)전혀 아니다", "(2)아니다.", "(3)그렇다.", "(4)매우그렇다." },
+                { "(1)전혀 아니다 (2)아니다. (3)그렇다. (4)매우그렇다." },
                 { "3.교수는 강의 내용에 대해 전문적 지식이 있었는가?" },
-                { "(1)전혀 아니다", "(2)아니다.", "(3)그렇다.", "(4)매우그렇다." },
+                { "(1)전혀 아니다 (2)아니다. (3)그렇다.(4)매우그렇다." },
                 { "4.강의 진행 속도는 적절하였는가?" },
-                { "(1)전혀 아니다", "(2)아니다.", "(3)그렇다.", "(4)매우그렇다." },
+                { "(1)전혀 아니다 (2)아니다. (3)그렇다. (4)매우그렇다." },
         };
         for (int first = 0; first < Que.length; first = first + 2) {
             System.out.println(Que[first][0]);
             for (int second = 0; second < Que[first].length; second = second + 2) {
-                // System.out.println(Que[second][Que.length]);
-                System.out.print(Que[second + 1][0]);
-                System.out.print(Que[second + 1][1]);
-                System.out.print(Que[second + 1][2]);
-                System.out.print(Que[second + 1][3]);
+                System.out.println(Que[second + 1][0]);
+                // System.out.print(Que[second + 1][0]);
+                // System.out.print(Que[second + 1][1]);
+                // System.out.print(Que[second + 1][2]);
+                // System.out.print(Que[second + 1][3]);
                 String anw = myOjt.nextLine();
                 System.out.println("답) " + anw);
                 answer[con] = anw;
