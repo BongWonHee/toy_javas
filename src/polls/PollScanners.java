c209fe9b54fa97edc6cb7a017f9b9059f739ec5d
SSSanga <ksa901125@gmail.com>
2023-05-31 19:23:01 +0900

    Get reply의 System out print deletion

--- src/polls/PollScanners.java
+++ src/polls/PollScanners.java
@@ -9,16 +9,18 @@ public class PollScanners {
     public PollScanners() {
     } // PollScanners 초기화
 
-    public PollScanners(int anw) {
-        Scanner myOjt = new Scanner(System.in);
+    // public PollScanners(int anw) {
+    //     Scanner myOjt = new Scanner(System.in);
+    //     anw = myOjt.nextInt();
+    //     this.reply = anw;
+    //     System.out.println("답 :" + anw);
+    // }
+public int Getreply (int anw) {
+    Scanner myOjt = new Scanner(System.in);
         anw = myOjt.nextInt();
         this.reply = anw;
-        System.out.println("답 :" + anw);
-    }
-public int Getreply () {
-    return this.reply;
-}
-}
+return this.reply;
+}}
 // Scanner myOjt//name =myOjt
 // String [] answer
 // 정답입력->넘어간다, string[]answer 넣어줌.
