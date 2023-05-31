43293f6e640e6aae1dc20e577a30b40b50adcd48
BongWonHee <leeban1004@gmail.com>
2023-05-31 18:02:27 +0900

    파일명 생성

---
+++ src/MaxMins.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MaxMins {
+    // try {
+    //     Scanner my = new Scanner(System.in);
+    //     int first = 0;
+    //     int second = 0 ;
+    //     int answer = Math.max(first, second);
+    //     System.out.println(answer);
+
+        
+    // } catch (Exception e) {
+    //     // TODO: handle exception
+    // }
+}
+
+
