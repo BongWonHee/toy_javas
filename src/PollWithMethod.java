53af5404e7f96dde7aaa153a1682838afb8d82ca
BongWonHee <leeban1004@gmail.com>
2023-06-01 06:03:38 +0900

    배열변경

--- src/PollWithMethod.java
+++ src/PollWithMethod.java
@@ -3,7 +3,7 @@
 import polls.PollInitailArray;
 import polls.PollScanners;
 import polls.PollStatistics;
-// 해당 코드는 PollScanners로 받은 입력값을 PollInitailArray로 받아 문제 출력후 다시 스캔을 받아서 PollStatistics로 넘겨 준 후 해당 값 출력 한다.
+//  PollInitailArray로 문제와 질문지를 출력후 입력값을 스캔을 받아서 PollStatistics로 넘겨 준 후 해당 값 출력 한다.
 public class PollWithMethod {
     public static void main(String[] args) {
         int x = 0;
@@ -12,8 +12,10 @@ public class PollWithMethod {
         PollInitailArray array = new PollInitailArray();
         PollStatistics statistics = new PollStatistics();
         try {
-            int z = scanners.scanners(x); //입력값은 1~4 까지로 제안한다. 
-            array.Arrays(z);// 스캐너에서 받은 값을 Array로 넘겨줌.
+            
+            
+            // int z = scanners.scanners(x); //입력값은 1~4 까지로 제안한다. 
+            array.Arrays(x);// 스캐너에서 받은 값을 Array로 넘겨줌.
            
 
         } catch (Exception e) {
